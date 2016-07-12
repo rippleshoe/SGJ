088139518316888e55ac386ff2371dc2cdf3065b
charles <Charles@CHARLES-INWIN>
2016-07-12 09:54:54 +0100

    Added inital tower model to scenes

---
+++ Assets/Scripts/hexGenScripts/DuplicateMap.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuplicateMap : MonoBehaviour {
+
+    public GameObject hexGrid;
+
+	void Start()
+    {
+        UnityEditor.PrefabUtility.CreatePrefab("prefabs", hexGrid);
+    }
+
+    
+}
